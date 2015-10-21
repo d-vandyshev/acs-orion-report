bbfe0c355dd0ba8b71ead221ff3c64bdfa3f3786
Dmitry Vandyshev <dmit.van@gmail.com>
2015-10-21 06:49:10 +0500

    Start main func and sample config

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"os"
+	"io"
+	"fmt"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+)
+
+const (
+	CONFIG_FILE = "web_access_acs_orion.conf"
+)
+
+type (
+	tomlConfig struct {
+		DBPath string
+	}
+)
+
+func main() {
+	// Logging
+	setLogging()
+
+	// Configuration
+	config := readConfig(CONFIG_FILE)
+
+	fmt.Println(config.DBPath)
+}
+
+func setLogging() {
+	f, err := os.OpenFile(getPath("log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		e(err)
+	}
+	defer f.Close()
+	log.SetOutput(io.MultiWriter(f, os.Stdout))
+}
+
+func readConfig(filename string) (config tomlConfig) {
+	_, err := toml.DecodeFile(CONFIG_FILE, &config)
+	if err != nil {
+		e(err)
+	}
+	return
+}
+
+func getPath(newExt string) (path string) {
+	base := filepath.Base(os.Args[0])
+	ext := filepath.Ext(os.Args[0])
+	name := base[:len(base)-len(ext)]
+	path = filepath.Dir(os.Args[0]) + string(os.PathSeparator) + name + "." + newExt
+	return
+}
+
+func e(m interface{}) {
+	log.Println(m)
+	log.Println("Exit by error")
+	os.Exit(1)
+}
